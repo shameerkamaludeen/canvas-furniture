07d46614953a32a0f2246f8d732d681b392c6e93
Shameer Kamaludeen <shameer.plsn@gmail.com>
2023-05-18 10:46:18 +0530

    remove unused blocks and components from the website

--- extension/blocks/contact/contact-map-form/contact-map-form.js
+++
@@ -1,4 +0,0 @@
-/* || Block - Contact us with map and form
-	 ========================================================================== */
-
-export { default } from '../../../components/form/form-radiused/form-radiused.js';
